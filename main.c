ee3795586a87668a6cee0142be38f6fedba88d70
Muzaffer-Irmak-Yavuz <muzafferirmaky@gmail.com>
2022-02-16 11:48:20 +0300

    Fixed spaces in help menu text

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 
 void help_menu(const char *const name)
 {
-    printf("Usage %s [options] [ <host name> <path to post data> <data>]\n",name);
-    printf("Options:\n");
+    printf("\nUsage %s [options] [ <host name> <path to post data> <data>]\n",name);
+    printf("Options:\n\n");
     printf("%s      Display this information\n","-h");
     printf("%s      Send text\n","-t");
 }
